f73dd4b6d037c87fb5f72a20e0c7f80b8216fd83
rv941 <rv941@example.com>
2026-03-29 13:24:28 +0530

    feat: configure google auth with android and web client ids

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,6 +7,14 @@ const config: CapacitorConfig = {
   server: {
     url: 'https://bible-gpt-ebon.vercel.app/',
     allowNavigation: ['bible-gpt-ebon.vercel.app']
+  },
+  plugins: {
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      androidClientId: '1083543499729-6h6e96849m3nn9qe8ir4ugc21djr1rlu.apps.googleusercontent.com',
+      clientId: '1083543499729-smnbok05h0g6gl25e3tetfokigs4edqv.apps.googleusercontent.com',
+      forceCodeForRefreshToken: true,
+    },
   }
 };
 

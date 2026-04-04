61297cce86ef7ceb61ca33698d6f9c9e48be51e3
rv941 <rv941@example.com>
2026-04-04 14:08:42 +0530

    Register SocialLoginPlugin in MainActivity to support scopes

--- android/app/src/main/java/com/biblenova/app/MainActivity.java
+++ android/app/src/main/java/com/biblenova/app/MainActivity.java
@@ -1,6 +1,13 @@
 package com.biblenova.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import ee.forgr.capacitor.social.login.SocialLoginPlugin;
 
 public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        registerPlugin(SocialLoginPlugin.class);
+    }
 }

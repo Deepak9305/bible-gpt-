867c74c9b77513e07824e7d30e4bc4b81b3d5a24
rv941 <rv941@example.com>
2026-04-04 16:51:25 +0530

    Implement ModifiedMainActivityForSocialLoginPlugin to fix scopes requirement

--- android/app/src/main/java/com/biblenova/app/MainActivity.java
+++ android/app/src/main/java/com/biblenova/app/MainActivity.java
@@ -1,13 +1,32 @@
 package com.biblenova.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import ee.forgr.capacitor.social.login.ModifiedMainActivityForSocialLoginPlugin;
 import ee.forgr.capacitor.social.login.SocialLoginPlugin;
 
-public class MainActivity extends BridgeActivity {
+public class MainActivity extends BridgeActivity implements ModifiedMainActivityForSocialLoginPlugin {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         registerPlugin(SocialLoginPlugin.class);
+    }
+
+    @Override
+    public void IHaveModifiedTheMainActivityForTheUseWithSocialLoginPlugin() {
+        // Required by Capgo SocialLoginPlugin
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        SocialLoginPlugin socialLoginPlugin = (SocialLoginPlugin) this.getBridge().getPlugin("SocialLogin")
+                .getInstance();
+        if (socialLoginPlugin != null) {
+            socialLoginPlugin.handleGoogleLoginIntent(requestCode, data);
+            socialLoginPlugin.handleAppleLoginIntent(data);
+        }
     }
 }
